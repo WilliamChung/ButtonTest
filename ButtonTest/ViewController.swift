53b790006673ce4236ca2731a1648b8511c80218
WilliamChung <William@Williams-Mac-mini.local>
2016-10-24 21:41:28 -0700

    UI and Basic Functions

--- ButtonTest/ViewController.swift
+++ ButtonTest/ViewController.swift
@@ -10,10 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeStamp: UITextField!
+    @IBOutlet weak var downDownTime: UITextField!
+    @IBOutlet weak var downUpTime: UITextField!
+    @IBOutlet weak var shiftButton: UIButton!
+    @IBOutlet weak var clickButton: UIButton!
+    
+    var tStamp: String {
+        return "\(NSDate().timeIntervalSince1970 * 1000)"
+    }
+    
+    var oldDownTime: Double = 0
+    var currentDownTime: Double = 0
+    var upTime: Double = 0
+    var downTimeDifference: Double = 0
+    var upTimeDifference: Double = 0
+    var shiftClicked: Bool = false
+    
+    @IBAction func push(_ sender: UIButton) {
+        
+        oldDownTime = currentDownTime
+        currentDownTime = Double (tStamp)!
+        
+        downTimeDifference = currentDownTime - oldDownTime
+        
+        downDownTime.text = String (downTimeDifference)
+        timeStamp.text = String (tStamp)
+        
+    }
+    
+    
+    @IBAction func release(_ sender: UIButton) {
+        
+        upTime = Double (tStamp)!
+        upTimeDifference = upTime - currentDownTime
+        
+        downUpTime.text = String (upTimeDifference)
+    }
+    
+    
+    @IBAction func shiftClick(_ sender: UIButton) {
+        
+        if(shiftClicked == false){
+            clickButton.setTitle("CLICK", for: .normal)
+            shiftClicked = true
+        }
+        else {
+            clickButton.setTitle("Click", for: .normal)
+            shiftClicked = false
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
